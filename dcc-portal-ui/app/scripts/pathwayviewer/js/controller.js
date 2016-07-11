11a4f0d26f719fdfb88578e307a9abc6d529f88b
jweiser <joel.weiser@oicr.on.ca>
2016-07-11 10:51:53 -0400

    whitespace changes

--- dcc-portal-ui/app/scripts/pathwayviewer/js/controller.js
+++ dcc-portal-ui/app/scripts/pathwayviewer/js/controller.js
@@ -240,7 +240,7 @@ angular.module('icgc.pathwayviewer.directives.controller', ['icgc.pathwayviewer.
        */
       ReactomePathway.prototype.highlight = function (rawMutationHighlights, rawDrugHighlights, overlaps) {
          var nodesInPathway = this.nodesInPathway;
-        _.keys(overlaps).forEach(function (dbId) {
+         _.keys(overlaps).forEach(function (dbId) {
             // Only highlight overlaps it if it's part of the pathway we're zooming in on
             // And only hide parts of it we are zooming in on a pathway
             if((nodesInPathway.length !== 0 && ! _.contains(nodesInPathway,dbId))){
