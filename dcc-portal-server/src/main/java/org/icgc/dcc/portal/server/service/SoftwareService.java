843da1d9d66a989e54232e811741fef83a217aca
GavinMWilson <Gavin.M.Wilson@oicr.on.ca>
2016-07-20 10:24:24 -0400

    Fixes to software page

--- dcc-portal-server/src/main/java/org/icgc/dcc/portal/server/service/SoftwareService.java
+++ dcc-portal-server/src/main/java/org/icgc/dcc/portal/server/service/SoftwareService.java
@@ -15,7 +15,7 @@
  * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN                         
  * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package org.icgc.dcc.portal.service;
+package org.icgc.dcc.portal.server.service;
 
 import java.net.URL;
 import java.util.List;
