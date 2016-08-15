e7e92dcd9421aea682760e5cca08214bae3bcc57
GavinMWilson <Gavin.M.Wilson@oicr.on.ca>
2016-08-15 16:06:15 -0400

    Refacotred code back to use @JsonValue

--- dcc-portal-server/src/main/java/org/icgc/dcc/portal/server/service/SoftwareService.java
+++ dcc-portal-server/src/main/java/org/icgc/dcc/portal/server/service/SoftwareService.java
@@ -20,14 +20,17 @@ package org.icgc.dcc.portal.server.service;
 import static com.google.common.base.Preconditions.checkState;
 
 import java.net.URL;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.icgc.dcc.portal.server.config.ServerProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.annotation.JsonValue;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.Data;
@@ -174,7 +177,18 @@ public class SoftwareService {
 
   public class Version implements Comparable<Version> {
 
-    public String version;
+    private String version;
+
+    @JsonValue
+    public final Map<String, String> getJson() {
+      Map<String, String> map = new HashMap<String, String>();
+      map.put("version", this.version);
+      return map;
+    }
+
+    public final String get() {
+      return this.version;
+    }
 
     public Version(String version) {
       checkState(version != null);
