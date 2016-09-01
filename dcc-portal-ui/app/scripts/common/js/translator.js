49105eb50941eaeb1bf887793809d28c56831a9c
Sid Joshi <sidjoshi@hotmail.ca>
2016-09-01 14:01:55 -0400

    Search result type now goes through the `trans` filter. Added unicode for space in 'trans' filter function so that the string doest break into new line. (#109)

--- dcc-portal-ui/app/scripts/common/js/translator.js
+++ dcc-portal-ui/app/scripts/common/js/translator.js
@@ -38,7 +38,7 @@
      function humanReadable(str) {
        var res = str;
        if (_.isEmpty(res) === true) { return res; }
-       res = res.replace(/_/g, ' ').replace(/^\s+|\s+$/g, '');
+       res = res.replace(/_/g, '\u00A0').replace(/^\s+|\s+$/g, '');
        res = res.charAt(0).toUpperCase() + res.slice(1);
        return res;
      }
