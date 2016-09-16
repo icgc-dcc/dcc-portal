a15ee20797f56e545135180964458beae40a6363
Chang Wang <garnwraly@gmail.com>
2016-09-16 16:48:56 -0400

    Don’t open manifest downloads in new tab

--- dcc-portal-ui/app/scripts/repository/js/repository.js
+++ dcc-portal-ui/app/scripts/repository/js/repository.js
@@ -363,9 +363,7 @@
           unique: $scope.shouldDeduplicate,
         });
 
-        var newTab = $window.open(manifestUrl);
-        var waitMessage = gettextCatalog.getString('Please wait while your file is being generated...');
-        newTab.document.body.innerHTML = '<html> <head> <title>Please wait for download | ICGC Data Portal</title> </head> <body > <style> body {display: flex; justify-content: center; align-items: center; flex-direction: column; font-family: Open Sans, sans-serif; background: #f7f7f7; color: #787878; } .spinner {-webkit-animation: rotate 2s linear infinite; animation: rotate 2s linear infinite; z-index: 2; width: 50px; height: 50px; margin-top: -56px; } .spinner .path {stroke: #d6d6d6; stroke-linecap: round; -webkit-animation: dash 1.5s ease-in-out infinite; animation: dash 1.5s ease-in-out infinite; } @-webkit-keyframes rotate {100% {-webkit-transform: rotate(360deg); transform: rotate(360deg); } } @keyframes rotate {100% {-webkit-transform: rotate(360deg); transform: rotate(360deg); } } @-webkit-keyframes dash {0% {stroke-dasharray: 1, 150; stroke-dashoffset: 0; } 50% {stroke-dasharray: 90, 150; stroke-dashoffset: -35; } 100% {stroke-dasharray: 90, 150; stroke-dashoffset: -124; } } @keyframes dash {0% {stroke-dasharray: 1, 150; stroke-dashoffset: 0; } 50% {stroke-dasharray: 90, 150; stroke-dashoffset: -35; } 100% {stroke-dasharray: 90, 150; stroke-dashoffset: -124; } }</style> <link href="https://fonts.googleapis.com/css?family=Open+Sans:400italic,400,300,700,700italic" rel="stylesheet" type="text/css"> <span>' + waitMessage + '</span> <h1 id="brand" class="animated fadeInDownEnter" style="font-size: 25px; font-weight: 300; margin-top: 40px; "> <img src="https://dcc.icgc.org/styles/images/icgc-logo.png" style="width: 30px; "> </h1><svg class="spinner" viewBox="0 0 50 50"> <circle class="path" cx="25" cy="25" r="20" fill="none" stroke-width="3"></circle> </svg> </body> </html>'; // jshint ignore:line
+        window.location.href = manifestUrl;
       } else {
         ExternalRepoService.downloadSelected($scope.selectedFiles, $scope.selectedRepos);
       }
