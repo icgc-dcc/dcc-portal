4f49cecdebb0898ffb4f5c3a9f6cf2c9d2282c28
Chang Wang <garnwraly@gmail.com>
2016-09-29 11:31:13 -0400

    explicitly set $ICGC_DEV_CONFIG to global

--- dcc-portal-ui/app/develop/scripts/config.js
+++ dcc-portal-ui/app/develop/scripts/config.js
@@ -1,6 +1,6 @@
 // Global $ICGC_DEV_CONFIG used for dev environment
 // profile
-var $ICGC_DEV_CONFIG = {
+global.$ICGC_DEV_CONFIG = {
 	// Set DEVELOPER_LOCAL_MODE to true to turn on local developer profile 
 	// for the 'grunt server' task
 	DEVELOPER_LOCAL_MODE: true, 
