e61c4ef753ef128da06e64a718b960031fe40847
Chang Wang <garnwraly@gmail.com>
2016-10-04 16:36:27 -0400

    Add <portal-settings> to index.html, swap out it in dev mode

---
+++ dcc-portal-ui/config/ICGC_SETTINGS.dev.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "releaseDate": "April 25th, 2016",
+    "dataVersion": 2,
+    "downloadEnabled": true,
+    "authEnabled": true,
+    "ssoUrl": "https://icgc.org/user?continue=",
+    "ssoUrlGoogle": "https://icgc.org/gooapi/redirect?continue=",
+    "maxNumberOfHits": 20000,
+    "maxMultiplier": 3,
+    "mirror": {
+        "enabled": false,
+        "countryCode": "",
+        "name": "",
+        "countryLocation": ""
+    },
+    "featureFlags": {
+        "AUTH_TOKEN": true,
+        "ICGC_CLOUD": true,
+        "VCF_IOBIO": true,
+        "SOFTWARE_PAGE": true
+    }
+};
