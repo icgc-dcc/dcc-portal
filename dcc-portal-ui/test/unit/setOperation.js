f6c5d379b29874a1f5180def7fc3d378d0885e9c
Chang Wang <garnwraly@gmail.com>
2016-10-26 11:19:41 -0400

    typo:semi in require string

--- dcc-portal-ui/test/unit/setOperation.js
+++ dcc-portal-ui/test/unit/setOperation.js
@@ -14,7 +14,7 @@
  * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
  * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-const innertext = require('innertext;');
+const innertext = require('innertext');
 
 describe('Test SetOperationService', function() {
   var SetOperationService;
