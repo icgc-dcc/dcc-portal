32adc1779fc8f71e08f13e3a4cd1a22aec72aa77
Sid Joshi <sidjoshi@hotmail.ca>
2016-11-01 16:48:13 -0400

    New oncogrid JS, added track legend label as suggested in the ticket, increased font size

--- dcc-portal-ui/app/scripts/oncogrid/js/directive.js
+++ dcc-portal-ui/app/scripts/oncogrid/js/directive.js
@@ -263,6 +263,7 @@
             minCellHeight: 8,
             trackHeight: 12,
             trackLegends: trackLegends,
+            trackLegendLabel: '(Legend)',
             donorTracks: donorTracks,
             donorOpacityFunc: donorOpacity,
             donorClick: donorClick,
