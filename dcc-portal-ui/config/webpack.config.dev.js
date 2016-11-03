efb5817e3cfc3387f178e58c50b0901005e49f2d
Chang Wang <garnwraly@gmail.com>
2016-11-03 17:40:51 -0400

    webpack: use raw loader for html files

--- dcc-portal-ui/config/webpack.config.dev.js
+++ dcc-portal-ui/config/webpack.config.dev.js
@@ -42,8 +42,9 @@ module.exports = {
     ],
     loaders: [
       {
-        test: /index.html$/,
-        loader: 'raw',
+        test: /\.html$/,
+        include: paths.appSrc,
+        loaders: ['raw']
       },
       {
         test: /index.html$/,
