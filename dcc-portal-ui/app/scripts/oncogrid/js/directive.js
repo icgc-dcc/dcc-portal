f169bf718b6821a7f78204fd2218ad179556d341
Sid Joshi <sidjoshi@hotmail.ca>
2016-11-07 09:58:20 -0500

    update to onco.js to add track legend label as an icon. using icgc help icon as track legend label.

--- dcc-portal-ui/app/scripts/oncogrid/js/directive.js
+++ dcc-portal-ui/app/scripts/oncogrid/js/directive.js
@@ -263,7 +263,7 @@
             minCellHeight: 8,
             trackHeight: 12,
             trackLegends: trackLegends,
-            trackLegendLabel: '(Legend)',
+            trackLegendLabel: '<i class="icon-help"></i>',
             donorTracks: donorTracks,
             donorOpacityFunc: donorOpacity,
             donorClick: donorClick,
