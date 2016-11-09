11df9d6fa7cc48897730dc3b6fabda80201d500a
Sid Joshi <sidjoshi@hotmail.ca>
2016-11-09 12:16:22 -0500

    remove webpack config for dev breakpoint

--- dcc-portal-ui/config/webpack.config.dev.js
+++ dcc-portal-ui/config/webpack.config.dev.js
@@ -5,7 +5,7 @@ var CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 var paths = require('./paths');
 
 module.exports = {
-  devtool: process.env.SOURCE_MAP ? process.env.SOURCE_MAP : 'cheap-module-eval-source-map',
+  devtool: 'cheap-module-eval-source-map',
   cache: true,
   context: path.resolve(__dirname, '../app/scripts'),
   entry: {
