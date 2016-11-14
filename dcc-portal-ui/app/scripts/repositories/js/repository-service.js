640697340d2e5a7946501f41b455f73db4fa9267
Chang Wang <garnwraly@gmail.com>
2016-11-14 11:36:01 -0500

    cleanup repository-service, remove unused variables

--- dcc-portal-ui/app/scripts/repositories/js/repository-service.js
+++ dcc-portal-ui/app/scripts/repositories/js/repository-service.js
@@ -5,12 +5,7 @@ const memoizeAsync = fn => memoize(fn, {promise: true});
 
 export default function (Restangular) {
   "ngInject";
-  
-  const repos = [];
-  const repoCodeMap = undefined;
-
-  const fetchRepos = () => Restangular.one('/repositories').get();
-
+  const fetchRepos = () => Restangular.one('/repositories').get().then(x => x.plain());
   this.getRepos = memoizeAsync(() => fetchRepos());
   this.getRepoCodeMap = memoizeAsync(() => this.getRepos().then(repos => keyBy(repos, 'code')));
 
