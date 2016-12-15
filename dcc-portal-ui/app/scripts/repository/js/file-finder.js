1e4809d28d7ee39a9adc85cbb4c5684abad4fe62
Chang Wang <garnwraly@gmail.com>
2016-12-15 13:55:45 -0800

    add show all button and prompt

--- dcc-portal-ui/app/scripts/repository/js/file-finder.js
+++ dcc-portal-ui/app/scripts/repository/js/file-finder.js
@@ -10,24 +10,50 @@ ngModule.component('fileFinder', {
         <tr ng-if="!vm.allFiles.length">
           <td class="text-center"><i class="icon-spinner icon-spin"></i>Loading</td>
         </tr>
-        <tr ng-repeat="result in vm.results">
+        <tr ng-repeat="result in vm.results | limitTo: vm.getLimit()">
           <td style="font-size: 1.1rem;">
             <i class="icon-file"></i>
-            <a target="_self" href="{{vm.ApiBaseurl}}/download?fn={{result.original.name}}" ng-bind-html="result.string"></a>
+            <a target="_self" href="{{:: vm.ApiBaseurl}}/download?fn={{:: result.original.name}}" ng-bind-html=":: result.string"></a>
           </td>
         </tr>
-        <tr ng-if="!vm.results.length">
+        <tr ng-if="vm.allFiles.length && !vm.results.length">
           <td class="text-center">
             <strong>No matching files found.</strong>
+          </td>
+        </tr>
+        <tr>
+          <td
+            ng-if="vm.results.length > vm.defaultLimit && vm.shouldShowAll === false"
+            ng-click="vm.shouldShowAll = true"
+            style="cursor: pointer; padding-left: 0.8em;"
+            tooltip="{{vm.results.length > vm.dangerZone ? 'Are you sure? The page might freeze for a few seconds' : '' }}"
+          >
+            <i
+              class="fa fa-arrow-down"
+              ng-class="{
+                'fa-arrow-down': !(vm.results.length > vm.dangerZone),
+                'fa-exclamation-triangle': vm.results.length > vm.dangerZone,
+              }"
+              ng-style="{
+                color: vm.results.length > vm.dangerZone ? '#d18686' : 'inherit'
+              }"
+            ></i>
+            Show All ({{vm.results.length.toLocaleString()}})
+            
           </td>
         </tr>
       </table>
     </div>
   `,
   controller: function (FileService, API) {
+    this.defaultLimit = 20;
+    this.dangerZone = 500;
     this.allFiles = [];
     this.results = [];
     this.ApiBaseurl = API.BASE_URL;
+    this.shouldShowAll = false;
+
+    this.getLimit = () => this.shouldShowAll ? Infinity : this.defaultLimit;
 
     FileService.getAllFiles().then((files) => {
       this.allFiles = files;
@@ -41,10 +67,12 @@ ngModule.component('fileFinder', {
         pre: '<strong>',
         post: '</strong>',
         extract: item => item.name,
-      }).slice(0, 20);
+      });
+      this.shouldShowAll = false;
     };
 
     this.$onChanges = onUpdate;
+    this.shouldShowAll = false;
   },
   controllerAs: 'vm',
   bindings: {
