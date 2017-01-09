f8296fc6ca03216305eab44aa908c604275b521e
Chang Wang <garnwraly@gmail.com>
2017-01-09 11:52:06 -0500

    move ENTITY_TYPES, getDefaultSortOrder into own file

---
+++ dcc-portal-ui/app/scripts/sets/js/getDefaultSetSortOrder.js
@@ -0,0 +1,12 @@
+import invariant from 'invariant';
+import { ENTITY_TYPES } from './ENTITY_TYPES';
+
+export const getDefaultSetSortOrder = (setType) => {
+  invariant(_.values(ENTITY_TYPES).includes(setType), `'type' must be one of [${_.values(ENTITY_TYPES)}]`);
+  return {
+    [ENTITY_TYPES.FILE]: 'id',
+    [ENTITY_TYPES.DONOR]: 'ssmAffectedGenes',
+    [ENTITY_TYPES.MUTATION]: 'affectedDonorCountFiltered',
+    [ENTITY_TYPES.GENE]: 'affectedDonorCountFiltered',
+  }[setType];
+};
