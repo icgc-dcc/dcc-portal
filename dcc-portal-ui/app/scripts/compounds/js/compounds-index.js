932e0fb75c859690de993fb7d8409c53cde863f8
Chang Wang <garnwraly@gmail.com>
2017-01-10 10:57:57 -0500

    CompoundIndexService with getAll

--- dcc-portal-ui/app/scripts/compounds/js/compounds-index.js
+++ dcc-portal-ui/app/scripts/compounds/js/compounds-index.js
@@ -6,6 +6,13 @@ angular.module('icgc.compounds.index', [])
       reloadOnSearch: false,
     });
   })
+  .service('CompoundIndexService', function (Restangular) {
+    const getAll = () => Restangular.all('drugs').getList().then(Restangular.stripRestangular);
+
+    Object.assign(this, {
+      getAll,
+    });
+  })
   .component('compoundIndex', {
     template: `
       <div>
@@ -14,10 +21,23 @@ angular.module('icgc.compounds.index', [])
             <span class="t_badge t_badge__compound"></span>
             <span>Compounds</span>
           </h1>
+        </div>
+        <pre>
+          {{ vm.compounds | json }}
+        </pre>
       </div>
     `,
-    controller (Page) {
+    controller: function (Page, CompoundIndexService) {
       Page.setTitle('Compounds');
-    }
+
+      const update = async () => {
+        this.compounds = await CompoundIndexService.getAll()
+        console.log(this.compounds)
+      };
+
+      this.$onInit = update;
+      this.$onChanges = update;
+    },
+    controllerAs: 'vm',
   })
   ;
