0a9d378d24c77499b23302fcad62596dddac560e
Chang Wang <garnwraly@gmail.com>
2017-01-23 11:14:25 -0500

    refactor: move reusable components into common/components

--- dcc-portal-ui/app/scripts/common/components/index.js
+++ dcc-portal-ui/app/scripts/common/components/index.js
@@ -2,4 +2,6 @@ angular.module('app.common.components', []);
 
 require('./icgc-checkbox');
 require('./collapsible-text');
+require('./collapsible-wrapper');
+require('./togglable-term');
 require('./paginated-table');
