44f0151b2875cbc7ac0a4602aed2bca5b58f48d2
Chang Wang <garnwraly@gmail.com>
2017-07-20 10:42:44 -0400

    log tracking activities in development environment and don't submit

--- dcc-portal-ui/app/scripts/common/js/track.js
+++ dcc-portal-ui/app/scripts/common/js/track.js
@@ -2,6 +2,10 @@ const { mixpanel, ga } = global;
 const _ = require('lodash');
 
 const track = (eventCategory, { action, label, value, ...otherProperties }) => {
+  if (process.env.NODE_ENV === 'development') {
+    console.log('track', eventCategory, _.omitBy({ action, label, value, ...otherProperties }, _.isNil));
+    return;
+  }
   if (mixpanel) {
     mixpanel.track(eventCategory, _.omitBy({ action, label, value, ...otherProperties }, _.isNil));
   }
