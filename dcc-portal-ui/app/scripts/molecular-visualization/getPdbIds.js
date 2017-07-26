5e967ff74e07d4ebfb97adc2a42447d1b21084d8
Chang Wang <garnwraly@gmail.com>
2017-07-26 16:39:43 -0400

    filter out invalid pdb codes from array returned by getPdbIds

--- dcc-portal-ui/app/scripts/molecular-visualization/getPdbIds.js
+++ dcc-portal-ui/app/scripts/molecular-visualization/getPdbIds.js
@@ -1,4 +1,4 @@
-const queryPDB = uniprotId => fetch('https://www.rcsb.org/pdb/rest/search/?req=browser&sortfield=Resolution', {
+const queryPdb = uniprotId => fetch('https://www.rcsb.org/pdb/rest/search/?req=browser&sortfield=Resolution', {
   method: 'POST',
   headers: {
     'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
@@ -9,14 +9,18 @@ const queryPDB = uniprotId => fetch('https://www.rcsb.org/pdb/rest/search/?req=b
   </orgPdbQuery>`,
 }).then(response => response.text());
 
+const isValidPdbCode = string =>
+  string.match(/^[\d][\d\w]{3}$/) || string.match(/^pdb_[\d]{5}[\d\w]{3}$/);
+
 const getPdbIds = uniprotId =>
-  queryPDB(uniprotId)
+  queryPdb(uniprotId)
     .then(response => {
       return response.trim() === 'null'
         ? []
         : response
           .replace(/:(?:\d+)\n/g, '\n')
-          .split('\n');
+          .split('\n')
+          .filter(isValidPdbCode);
     });
 
 export default getPdbIds;
