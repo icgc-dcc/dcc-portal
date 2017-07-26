2ee696dbe12264a495a21df6859909b03bca19ac
Chang Wang <garnwraly@gmail.com>
2017-07-26 15:53:56 -0400

    Stop spin when user hovers over 3d structure, restore spin when mouse leaves

--- dcc-portal-ui/app/scripts/molecular-visualization/molecular-visualization.js
+++ dcc-portal-ui/app/scripts/molecular-visualization/molecular-visualization.js
@@ -8,12 +8,20 @@ angular.module('icgc.molecular.visualization', [])
       <div class="loading-message" ng-show="vm.loadState.isLoading">
         <i>.</i><i>.</i><i>.</i><i>.</i>
       </div>
-      <div class="viewport" style="width: 100%; height: 400px;"></div>
+      <div class="not-found-message" ng-if="vm.hasNoResult">
+        No matching structure found
+      </div>
+      <div
+        class="viewport"
+        style="width: 100%; height: 400px;"
+        ng-mouseenter="vm.onMouseEnter()"
+        ng-mouseleave="vm.onMouseLeave()"
+      ></div>
     `,
     bindings: {
       uniprotId: '<',
     },
-    controller: function ($scope, $element, LoadState) {
+    controller: function ($scope, $element, LoadState, $timeout) {
       var loadState = new LoadState();
       this.loadState = loadState;
       require.ensure([], (require) => {
@@ -21,6 +29,10 @@ angular.module('icgc.molecular.visualization', [])
         var stage = new NGL.Stage($element.find('.viewport').get(0), {
           backgroundColor: 'white',
         });
+
+        stage.spinAnimation.angle = 0;
+        stage.setSpin(true);
+
         const uniprotId = this.uniprotId[0];
         loadState.loadWhile(
           getPdbIds(uniprotId)
@@ -29,14 +41,46 @@ angular.module('icgc.molecular.visualization', [])
                 await stage.loadFile(`rcsb://${pdbIds[0]}`, {
                   defaultRepresentation: true,
                 });
-                stage.setSpin(true);
+                startSpin();
               } else {
-                console.log(`No pdb id found for ${uniprotId}`);
+                this.hasNoResult = true;
               }
             })
         );
+
         this.$onDestroy = () => stage.dispose();
+
+        const tweenr = require('tweenr')();
+        const startSpin = () => {
+          stage.setSpin(true);
+          const tween = tweenr.to(
+            { angle: 0 },
+            { angle: 0.001, duration: 1, ease: 'sineIn' });
+          tween.on('update', (tween) => {
+            stage.spinAnimation.angle = tween.target.angle;
+          });
+        };
+
+        const stopSpin = () => {
+          const tween = tweenr.to(
+            { angle: 0.001 },
+            { angle: 0, duration: 0.4, ease: 'expoOut' });
+          tween.on('update', (tween) => {
+            stage.spinAnimation.angle = tween.target.angle;
+          });
+          tween.on('complete', () => stage.setSpin(false));
+        };
+
+        let interactionTimeout;
+        this.onMouseEnter = () => {
+          $timeout.cancel(interactionTimeout);
+          stopSpin();
+        };
+        this.onMouseLeave = () => {
+          interactionTimeout = $timeout(startSpin, 0.3 * 1000);
+        };
       });
+
     },
     controllerAs: 'vm',
   });
