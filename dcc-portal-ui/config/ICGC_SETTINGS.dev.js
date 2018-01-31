1ef8679ffd0db05ade5adb60d49043d8f4fb243a
Alex <lepsalex@gmail.com>
2018-01-31 12:56:42 -0500

    Jupyter settings

--- dcc-portal-ui/config/ICGC_SETTINGS.dev.js
+++ dcc-portal-ui/config/ICGC_SETTINGS.dev.js
@@ -1,24 +1,28 @@
 /* eslint-disable quotes */
 
 module.exports = {
-    "releaseDate": "April 25th, 2016",
-    "dataVersion": 2,
-    "downloadEnabled": true,
-    "authEnabled": true,
-    "ssoUrl": "https://icgc.org/user?continue=",
-    "ssoUrlGoogle": "https://icgc.org/gooapi/redirect?continue=",
-    "maxNumberOfHits": 20000,
-    "maxMultiplier": 3,
-    "mirror": {
-        "enabled": false,
-        "countryCode": "",
-        "name": "",
-        "countryLocation": ""
-    },
-    "featureFlags": {
-        "AUTH_TOKEN": true,
-        "ICGC_CLOUD": true,
-        "VCF_IOBIO": true,
-        "SOFTWARE_PAGE": true
-    }
+  releaseDate: 'April 25th, 2016',
+  dataVersion: 2,
+  downloadEnabled: true,
+  authEnabled: true,
+  ssoUrl: 'https://icgc.org/user?continue=',
+  ssoUrlGoogle: 'https://icgc.org/gooapi/redirect?continue=',
+  maxNumberOfHits: 20000,
+  maxMultiplier: 3,
+  mirror: {
+    enabled: false,
+    countryCode: '',
+    name: '',
+    countryLocation: '',
+  },
+  featureFlags: {
+    AUTH_TOKEN: true,
+    ICGC_CLOUD: true,
+    VCF_IOBIO: true,
+    SOFTWARE_PAGE: true,
+  },
+  jupyter: {
+    enabled: true,
+    url: 'https://jupyterhub.cancercollaboratory.org:8000',
+  },
 };
