1ab1277c38af7d313f7c5a166a2bc2c2abcbdd8a
Alex <lepsalex@gmail.com>
2018-02-12 14:04:43 -0500

    Babel prod updated for dynamic imports

--- dcc-portal-ui/config/babel.prod.js
+++ dcc-portal-ui/config/babel.prod.js
@@ -1,10 +1,11 @@
 module.exports = {
   babelrc: false,
-  presets: [
-    'babel-preset-es2015',
-    'babel-preset-es2016',
-  ].map(require.resolve),
+  presets: ['babel-preset-react', 'babel-preset-es2015', 'babel-preset-es2016'].map(
+    require.resolve
+  ),
   plugins: [
+    'babel-plugin-syntax-dynamic-import',
+    'babel-plugin-dynamic-import-webpack',
     'babel-plugin-transform-decorators-legacy',
     'babel-plugin-transform-decorators',
     'babel-plugin-transform-class-properties',
@@ -13,12 +14,17 @@ module.exports = {
     'babel-plugin-transform-object-rest-spread',
     'babel-plugin-add-module-exports',
     'babel-plugin-transform-async-to-generator',
-  ].map(require.resolve).concat([
-    [require.resolve('babel-plugin-transform-runtime'), {
-      helpers: false,
-      polyfill: false,
-      regenerator: true
-    }],
-    ['babel-root-import', { 'rootPathSuffix': 'app' }],
-  ])
+  ]
+    .map(require.resolve)
+    .concat([
+      [
+        require.resolve('babel-plugin-transform-runtime'),
+        {
+          helpers: false,
+          polyfill: false,
+          regenerator: true,
+        },
+      ],
+      ['babel-root-import', { rootPathSuffix: 'app' }],
+    ]),
 };
