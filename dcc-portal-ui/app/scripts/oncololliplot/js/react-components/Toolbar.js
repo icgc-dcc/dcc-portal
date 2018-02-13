394b0134d2bfd1c61573a92479017f1d794ec630
Alex <lepsalex@gmail.com>
2018-02-13 10:53:25 -0500

    Toolbar Styling

--- dcc-portal-ui/app/scripts/oncololliplot/js/react-components/Toolbar.js
+++ dcc-portal-ui/app/scripts/oncololliplot/js/react-components/Toolbar.js
@@ -3,25 +3,36 @@ import { connect } from 'react-redux';
 import { object, array, func } from 'prop-types';
 import { loadTranscript, reset } from '../redux/OncoLolliplot/redux';
 
-
-const Toolbar = ({ loadTranscript, reset, transcripts, selectedTranscript, mutationService, filters }) => {
-
+const Toolbar = ({
+  loadTranscript,
+  reset,
+  transcripts,
+  selectedTranscript,
+  mutationService,
+  filters,
+}) => {
   const selectTranscript = id => {
     const t = transcripts.filter(t => t.id === id)[0];
     loadTranscript({ selectedTranscript: t, mutationService, filters });
-  }
+  };
 
   return (
     <div className="lolliplot-toolbar">
-      <strong>Transcript: </strong>
-      <select onChange={e => selectTranscript(e.target.value)} value={selectedTranscript.id}>
+      <strong>Transcript:</strong>
+      <select
+        style={{ marginLeft: '6px' }}
+        onChange={e => selectTranscript(e.target.value)}
+        value={selectedTranscript.id}
+      >
         {transcripts.map(t => (
           <option key={t.id} value={t.id}>
             {t.name} ({t.lengthAminoAcid} aa)
           </option>
         ))}
       </select>
-      <button onClick={reset}>Reset</button>
+      <span onClick={reset} style={{ color: '#e9931c', marginLeft: '10px', cursor: 'pointer' }}>
+        <i className="icon-undo" />Reset
+      </span>
     </div>
   );
 };
@@ -40,7 +51,7 @@ const mapStateToProps = state => {
     transcripts: state.oncoLolliplot.transcripts,
     selectedTranscript: state.oncoLolliplot.selectedTranscript,
     mutationService: state.oncoLolliplot.mutationService,
-    filters: state.oncoLolliplot.filters
+    filters: state.oncoLolliplot.filters,
   };
 };
 
