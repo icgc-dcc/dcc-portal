980f51e6f6cdd850461e36e12ab5ebc219b763d0
Alex <lepsalex@gmail.com>
2018-02-13 11:26:52 -0500

    Webpack optimization test

--- dcc-portal-ui/config/webpack.config.prod.js
+++ dcc-portal-ui/config/webpack.config.prod.js
@@ -9,32 +9,29 @@ module.exports = {
   bail: true,
   devtool: 'eval',
   entry: {
-    app: [
-      require.resolve('./polyfills'),
-      path.join(paths.appSrc, 'scripts/index')
-    ],
+    app: [require.resolve('./polyfills'), path.join(paths.appSrc, 'scripts/index')],
     vendor: path.join(paths.appSrc, 'scripts/vendor'),
   },
   output: {
     path: paths.appBuild,
     filename: 'static/js/[name].[chunkhash:8].js',
     chunkFilename: 'static/js/[name].[chunkhash:8].chunk.js',
-    publicPath: '/'
+    publicPath: '/',
   },
   resolve: {
     extensions: ['', '.js', '.json'],
   },
   resolveLoader: {
     root: paths.ownNodeModules,
-    moduleTemplates: ['*-loader']
+    moduleTemplates: ['*-loader'],
   },
   module: {
     preLoaders: [
       {
         test: /\.js$/,
         loader: 'eslint',
-        include: paths.appSrc
-      }
+        include: paths.appSrc,
+      },
     ],
     noParse: /node_modules\/lodash\/lodash\.js/,
     loaders: [
@@ -48,11 +45,11 @@ module.exports = {
         query: {
           multiple: [
             {
-              search: '\'COPYRIGHT_YEAR\'',
-              replace: new Date().getUTCFullYear()
+              search: "'COPYRIGHT_YEAR'",
+              replace: new Date().getUTCFullYear(),
             },
-          ]
-        }
+          ],
+        },
       },
       {
         test: /\.js$/,
@@ -75,15 +72,15 @@ module.exports = {
       {
         test: /\.json$/,
         include: [paths.appSrc, paths.appNodeModules],
-        loader: 'json'
+        loader: 'json',
       },
       {
         test: /\.(swf|jpg|png|gif|eot|svg|ttf|woff|woff2)(\?.*)?$/,
         include: [paths.appSrc, paths.appNodeModules],
         loader: 'file',
         query: {
-          name: 'static/media/[name].[hash:8].[ext]'
-        }
+          name: 'static/media/[name].[hash:8].[ext]',
+        },
       },
       {
         test: /\.(mp4|webm)(\?.*)?$/,
@@ -91,36 +88,34 @@ module.exports = {
         loader: 'url',
         query: {
           limit: 10000,
-          name: 'static/media/[name].[hash:8].[ext]'
-        }
-      }
-    ]
+          name: 'static/media/[name].[hash:8].[ext]',
+        },
+      },
+    ],
   },
   eslint: {
     // TODO: consider separate config for production,
     // e.g. to enable no-console and no-debugger only in prod.
     configFile: path.join(__dirname, 'eslint.js'),
-    useEslintrc: false
+    useEslintrc: false,
   },
   postcss: function() {
-    return [
-      require('autoprefixer'),
-    ];
+    return [require('autoprefixer')];
   },
   plugins: [
     new CopyWebpackPlugin([
-      {from: 'app/_redirects', to: paths.appBuild},
-      {from: 'app/robots.txt', to: paths.appBuild + '/robots.txt'},
-      {from: 'app/sitemap.xml', to: paths.appBuild + '/sitemap.xml'},
-      {from: 'app/opensearch.xml', to: paths.appBuild + '/opensearch.xml'},
-      {from: 'app/favicon.ico', to: paths.appBuild + '/favicon.ico'},
-      {from: 'app/styles/fonts', to: paths.appBuild + '/styles/fonts'},
-      {from: 'app/styles/images', to: paths.appBuild + '/styles/images'},
-      {from: 'vendor/styles', to: paths.appBuild + '/vendor/styles'},
-      {from: 'app/styles/icgc-icons.css', to: paths.appBuild + '/styles/icgc-icons.css'},
+      { from: 'app/_redirects', to: paths.appBuild },
+      { from: 'app/robots.txt', to: paths.appBuild + '/robots.txt' },
+      { from: 'app/sitemap.xml', to: paths.appBuild + '/sitemap.xml' },
+      { from: 'app/opensearch.xml', to: paths.appBuild + '/opensearch.xml' },
+      { from: 'app/favicon.ico', to: paths.appBuild + '/favicon.ico' },
+      { from: 'app/styles/fonts', to: paths.appBuild + '/styles/fonts' },
+      { from: 'app/styles/images', to: paths.appBuild + '/styles/images' },
+      { from: 'vendor/styles', to: paths.appBuild + '/vendor/styles' },
+      { from: 'app/styles/icgc-icons.css', to: paths.appBuild + '/styles/icgc-icons.css' },
 
       // TODO: this is only necessary due to templateUrls, we should compile templates
-      {from: 'app/scripts', to: paths.appBuild + '/scripts'},
+      { from: 'app/scripts', to: paths.appBuild + '/scripts' },
     ]),
     new HtmlWebpackPlugin({
       inject: true,
@@ -136,11 +131,11 @@ module.exports = {
         keepClosingSlash: true,
         minifyJS: true,
         minifyCSS: true,
-        minifyURLs: true
-      }
+        minifyURLs: true,
+      },
     }),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': '"production"',
+      'process.env.NODE_ENV': JSON.stringify('production'),
       'process.env.GENOME_VIEWER_REQUIRE_STRING': JSON.stringify(require('./shims/genome-viewer')),
     }),
     new webpack.optimize.OccurrenceOrderPlugin(),
@@ -148,17 +143,21 @@ module.exports = {
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         screw_ie8: true,
-        warnings: false
+        warnings: false,
+        dead_code: true,
       },
       mangle: {
-        screw_ie8: true
+        screw_ie8: true,
       },
       output: {
         comments: false,
-        screw_ie8: true
-      }
+        screw_ie8: true,
+      },
     }),
-    new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"vendor.bundle.js"),
+    new webpack.optimize.CommonsChunkPlugin(
+      /* chunkName= */ 'vendor',
+      /* filename= */ 'vendor.bundle.js'
+    ),
     new ExtractTextPlugin('static/css/[name].[contenthash:8].css'),
-  ]
+  ],
 };
