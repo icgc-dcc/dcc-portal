0845a905640527c5cf87f27215bc19bd8ba80a6e
Alex <lepsalex@gmail.com>
2018-02-13 14:42:24 -0500

    Dead code removal

--- dcc-portal-ui/config/babel.prod.js
+++ dcc-portal-ui/config/babel.prod.js
@@ -14,6 +14,7 @@ module.exports = {
     'babel-plugin-transform-object-rest-spread',
     'babel-plugin-add-module-exports',
     'babel-plugin-transform-async-to-generator',
+    'babel-plugin-transform-react-remove-prop-types',
   ]
     .map(require.resolve)
     .concat([
