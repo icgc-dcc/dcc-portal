ac18883c6274c68366dca805fec9123e53baa8f3
Alex <lepsalex@gmail.com>
2018-04-24 16:25:45 -0400

    Geneset tabbed entity page

--- dcc-portal-ui/app/scripts/genesets/js/genesets.js
+++ dcc-portal-ui/app/scripts/genesets/js/genesets.js
@@ -20,24 +20,38 @@
 
   var module = angular.module('icgc.genesets', ['icgc.genesets.controllers', 'ui.router']);
 
+  const stateResolver = {
+    geneSet: [
+      '$stateParams',
+      'GeneSets',
+      function($stateParams, GeneSets) {
+        return GeneSets.one($stateParams.id)
+          .get()
+          .then(function(geneSet) {
+            return geneSet;
+          });
+      },
+    ],
+  };
+
   module.config(function($stateProvider) {
     $stateProvider.state('geneset', {
       url: '/genesets/:id',
       templateUrl: 'scripts/genesets/views/geneset.html',
       controller: 'GeneSetCtrl as GeneSetCtrl',
-      resolve: {
-        geneSet: [
-          '$stateParams',
-          'GeneSets',
-          function($stateParams, GeneSets) {
-            return GeneSets.one($stateParams.id)
-              .get()
-              .then(function(geneSet) {
-                return geneSet;
-              });
-          },
-        ],
-      },
+      data: { tab: 'summary' },
+      resolve: stateResolver,
+    });
+
+    // [ {tab name}, {url} ] - consumed below by forEach
+    const tabs = [['variants', 'variants'], ['donors', 'donors'], ['pathwayViewer', 'pathway-viewer']];
+
+    tabs.forEach(tab => {
+      $stateProvider.state(`geneset.${tab[0]}`, {
+        url: `/${tab[1]}`,
+        data: { tab: `${tab[0]}` },
+        resolve: stateResolver,
+      });
     });
   });
 })();
@@ -83,6 +97,8 @@
 
     Page.setTitle(geneSet.id);
     Page.setPage('entity');
+
+    _ctrl.activeTab = $state.current.data.tab;
 
     _ctrl.geneSet = geneSet;
     _ctrl.geneSet.queryType = FiltersUtil.getGeneSetQueryType(_ctrl.geneSet.type);
